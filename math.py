fefe4eb4122717ef876012ee3293247578615477
benjamin <130307563+aigbonabenjamin@users.noreply.github.com>
2023-05-08 14:50:14 +0100

    Update math.py

--- math.py
+++ math.py
@@ -1,2 +1,18 @@
 class Math:
-    pass
+    def __init__(self):
+        pass
+    def add(num1, num2):
+        return num1 + num2
+    
+    def minus(num1, num2):
+        return num1 - num2
+    
+    def multiply(num1, num2):
+        return num1 * num2
+    
+    def divide(num1, num2):
+        return num1 / num2
+    
+    def exponent(num1, num2):
+        pass
+        
