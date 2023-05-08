215c055378c73d47b335ba69c4fa82b5b6e0e4d5
rokarioss <oshomahiyelobu1@gmail.com>
2023-05-08 14:47:45 +0100

    ajiri's contribution

--- physics.py
+++ physics.py
@@ -1,2 +1,15 @@
 class Physics:
-    pass
+    def __init__(self) -> None:
+        self.gravity = 9.8
+
+    def calc_speed(self, d, t):
+        self.speed = d / t
+        return self.speed
+    
+    def calc_PE(self, m, h):
+        pe = m * self.gravity * h
+        return pe
+    
+    def calc_force(self, m, a):
+        force = m * a
+        return force
