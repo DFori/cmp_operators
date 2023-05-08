200d9bfa976656f0568ebac2860e5970b0280940
Daniel Fori <122609602+DFori@users.noreply.github.com>
2023-05-08 14:33:19 +0100

    Add files via upload
    starter code

---
+++ main.py
@@ -0,0 +1,10 @@
+from math import Math
+from physics import Physics
+import art
+print(art.logo)
+print("Welcome to our Math and Physics operation machine")
+mth_or_phy = input("What operation would you like to carry out math or phycics\n> ")
+
+
+
+
